50bfbc8f94c603587d6308d0c8f6376c81733aa9
juliannade <juliannade@gmail.com>
2021-07-04 19:10:55 -0700

    Initial commit

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").classList.add("mystyle");
+}
+
+
+
+function closeNav() {
+  document.getElementById("mySidenav").classList.remove("mystyle");
+}
